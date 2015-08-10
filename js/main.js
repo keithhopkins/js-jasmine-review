c128bc3e147660c1c7ac8d675c529e0a4623ca56
keithhopkins <knhopkins412@gmail.com>
2015-08-10 08:35:31 -0600

    complete

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-function largestNumber(arr){
+var Statistics = function(){
+  this.name = 'stats';
+};
+
+Statistics.prototype.largestNumber=function(arr){
   var largest = arr[0];
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] > largest) {
@@ -6,8 +10,98 @@ function largestNumber(arr){
     }
   }
   return largest;
-}
-
-module.exports = {
-  largestNumber: largestNumber
 };
+
+
+Statistics.prototype.oddNumbers=function(num){
+  if(num<40)
+    return [num];
+  var odd=[];
+  for(var i=41;i<100;i++)
+    if(i%2===1&&i<num)
+      odd.push(i);
+  return odd;
+};
+
+Statistics.prototype.evenNumbers = function(num){
+  var even = [];
+  for(var i=0;i<=num;i++)
+    if(i%2===0)
+      even.push(i);
+  return even;
+};
+
+Statistics.prototype.allOdds = function(num){
+  var odds = [];
+  if(num<=40){
+    for(var i=40;i>=num;i--)
+      odds.push(i);
+  } else {
+    for(var k=41;k<=num;k+=2){
+      odds.push(k);
+    }
+  }
+  return odds;
+};
+
+Statistics.prototype.sumDigits = function(num){
+  if(num.toString().length===1)
+    return num;
+  var digits = num.toString().split('');
+  return digits.reduce(function(prevValue,curValue,index,array){
+    return Number(prevValue)+Number(curValue);
+  });
+};
+
+Statistics.prototype.sumDigitsToOne = function(num){
+  if(num.toString().length===1)
+    return num;
+  var digits = num.toString().split('');
+  return this.sumDigitsToOne(
+    digits.reduce(function(prevValue,curValue,index,array){
+    return Number(prevValue)+Number(curValue);
+  }));
+};
+
+Statistics.prototype.smallDivisor = function(num,num2){
+  for (var i=2;i<num;i++){
+    if(num%i===0&&num2%i===0){
+      return i;
+    }
+  }
+};
+
+Statistics.prototype.findLCM = function(num,num2){
+  var LCM = num;
+  while(true){
+    if(LCM%num===0&&LCM%num2===0)
+      return LCM;
+    LCM++;
+  }
+};
+
+Statistics.prototype.statisticalNumbers = function(nums){
+  var stats = {std: 0, average:0};
+  var avg = this.average(nums);
+  stats.average = avg;
+  var standardDev = this.standardDeviation(nums);
+  stats.std = standardDev;
+  return stats;
+};
+
+Statistics.prototype.standardDeviation = function(nums){
+  var mean = this.average(nums);
+  var sqMean = nums.map(function(curVal){
+    return Math.pow((curVal-mean),2);
+  });
+  sqMean = this.average(sqMean);
+  return Math.sqrt(sqMean);
+};
+
+Statistics.prototype.average = function(nums){
+  var avg = nums.reduce(function(prevVal,curVal,index,array){
+    return prevVal+curVal;
+  });
+  return avg/nums.length;
+};
+module.exports = Statistics;
--- spec/spec.js
+++ spec/spec.js
@@ -1,73 +1,76 @@
-var code = require('../js/main.js');
-
+var Statistics = require('../js/main.js');
+var stats = new Statistics();
 // Problem 1
 // Write a function that takes three random numbers from 0 to 100 and returns the largest one.
 
 describe('Problem 1', function() {
+  beforeEach(function(){
+    
+  });
   it('should return the largest number', function() {
-    expect(code.largestNumber([10, 20, 30])).toBe(30);
-    expect(code.largestNumber([1, 20, 3])).toBe(20);
-    expect(code.largestNumber([-1, -20, -3])).not.toBe(-20);
+    expect(stats.largestNumber([10, 20, 30])).toEqual(30);
+    expect(stats.largestNumber([1, 20, 3])).toEqual(20);
+    expect(stats.largestNumber([-1, -20, -3])).not.toEqual(-20);
   });
 });
 
 describe('Problem 2',function() {
   it('should return all the odd numbers from 40 to that number or return the number if its less than 40',function() {
-    expect(code.oddNumbers(50)).toBe([41,43,45,47,49]);
-    expect(code.oddNumbers(30)).toBe([30]);
+    expect(stats.oddNumbers(50)).toEqual([41,43,45,47,49]);
+    expect(stats.oddNumbers(30)).toEqual([30]);
   });
 });
 
 describe('Problem 3', function() {
   it('should return all the even numbers from 0 to input',function(){
-    expect(code.evenNumbers(13)).toBe([0,2,4,6,8,10,12]);
-    expect(code.evenNumbers(10)).toBe([0,2,4,6,8,10]);
+    expect(stats.evenNumbers(13)).toEqual([0,2,4,6,8,10,12]);
+    expect(stats.evenNumbers(10)).toEqual([0,2,4,6,8,10]);
   });
 });
 
 describe('Problem 4',function(){
-  it('should return all odd numbers from 40 to the input',function(){
-    expect(code.allOdds(49)).toBe([41,43,45,47,49]);
-    expect(code.allOdds(34)).toBe([40,39,38,37,36,35,34]);
+  it('should return all odd numbers from 40 to the input or all numbers from 40 to the number if its below 40',function(){
+    expect(stats.allOdds(49)).toEqual([41,43,45,47,49]);
+    expect(stats.allOdds(34)).toEqual([40,39,38,37,36,35,34]);
   });
 });
 
 describe('Problem 5',function(){
   it('should return the sum of a numbers digits',function(){
-    expect(code.sumDigits(94132)).toBe(19);
-    expect(code.sumDigits(1624)).toBe(13);
-    expect(code.sumDigits(3)).toBe(3);
+    expect(stats.sumDigits(94132)).toEqual(19);
+    expect(stats.sumDigits(1624)).toEqual(13);
+    expect(stats.sumDigits(3)).toEqual(3);
   });
 });
 
 describe('Problem 6',function(){
   it('should return the sum of a numbers digit until it is 1 digit',function(){
-    expect(code.sumDigits(1049)).toBe(5);
-    expect(code.sumDigits(4325)).toBe(5);
-    expect(code.sumDigits(9999)).toBe(9);
+    expect(stats.sumDigitsToOne(1049)).toEqual(5);
+    expect(stats.sumDigitsToOne(4325)).toEqual(5);
+    expect(stats.sumDigitsToOne(9999)).toEqual(9);
   });
 });
 
 describe('Problem 7',function(){
   it('should return the smallest common divisor greater than 1',function(){
-    expect(code.smallDivisor(9,6)).toBe(3);
-    expect(code.smallDivisor(30,60)).toBe(2);
-    expect(code.smallDivisor(35,55)).toBe(5);
+    expect(stats.smallDivisor(9,6)).toEqual(3);
+    expect(stats.smallDivisor(30,60)).toEqual(2);
+    expect(stats.smallDivisor(35,55)).toEqual(5);
   });
 });
 
 describe('Problem 8',function(){
   it('should return the LCM of 2 numbers',function(){
-    expect(code.findLCM(3,55)).toBe(155);
-    expect(code.findLCM(5,12)).toBe(60);
+    expect(stats.findLCM(3,55)).toEqual(165);
+    expect(stats.findLCM(5,12)).toEqual(60);
   });
 });
 
 describe('Problem 9',function(){
   it('should return 2 numbers average and standard deviation', function(){
-    expect(code.statistics([17,53])).toBe({std:20.5,average:32.5});
-    expect(code.statistics([67,73])).toBe({std:3,average:70});
-    expect(code.statistics([54,82])).toBe({std:14,average:68});
+    expect(stats.statisticalNumbers([17,53])).toEqual({std:18,average:35});
+    expect(stats.statisticalNumbers([67,73])).toEqual({std:3,average:70});
+    expect(stats.statisticalNumbers([54,82])).toEqual({std:14,average:68});
   });
 });
 
